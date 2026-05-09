8969e6acbee59edf2df1b08dc2c6e2e6d4310c90
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-10 00:17:52 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/csptingdockergh/CsptingdockerghApplication.java
@@ -0,0 +1,13 @@
+package com.example.csptingdockergh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsptingdockerghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsptingdockerghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csptingdockergh/CsptingdockerghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csptingdockergh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsptingdockerghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
